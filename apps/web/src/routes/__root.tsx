11f3c523f0a870af5eae2738edad1bf13c23e2f1
Shady Khalifa <dev+github@shadykhalifa.me>
2025-09-06 01:52:28 +0300

    initial commit

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,68 @@
+import { Toaster } from "@/components/ui/sonner";
+
+import {
+	HeadContent,
+	Outlet,
+	Scripts,
+	createRootRouteWithContext,
+	useRouterState,
+	useRouteContext,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Header from "../components/header";
+import appCss from "../index.css?url";
+import type { QueryClient } from "@tanstack/react-query";
+import type { ConvexQueryClient } from "@convex-dev/react-query";
+import type { ConvexReactClient } from "convex/react";
+import Loader from "@/components/loader";
+
+export interface RouterAppContext {
+	queryClient: QueryClient;
+	convexClient: ConvexReactClient;
+	convexQueryClient: ConvexQueryClient;
+}
+
+export const Route = createRootRouteWithContext<RouterAppContext>()({
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "My App",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: appCss,
+			},
+		],
+	}),
+
+	component: RootDocument,
+});
+
+function RootDocument() {
+	const isFetching = useRouterState({ select: (s) => s.isLoading });
+	return (
+		<html lang="en" className="dark">
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				<div className="grid h-svh grid-rows-[auto_1fr]">
+					<Header />
+					{isFetching ? <Loader /> : <Outlet />}
+				</div>
+				<Toaster richColors />
+				<TanStackRouterDevtools position="bottom-left" />
+				<Scripts />
+			</body>
+		</html>
+	);
+}
