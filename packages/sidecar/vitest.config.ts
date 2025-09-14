6d67b28a0732570f6d0550268982461ae22394cd
Shady Khalifa <dev+github@shadykhalifa.me>
2025-09-14 19:14:28 +0300

    config: update vitest environment to edge-runtime

--- packages/sidecar/vitest.config.ts
+++ packages/sidecar/vitest.config.ts
@@ -1,14 +1,8 @@
-import * as path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: "node",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    environment: "edge-runtime",
   },
 });
